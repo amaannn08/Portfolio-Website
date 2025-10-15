d056b57064f1476bf5df79cece6e7a6169df9157
linearmusic <amaannn.gupta.08@gmail.com>
2025-10-15 12:57:49 +0530

    Made the footer better

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,11 @@ const Footer = () => {
   return (
     <div className='flex felx-row justify-between px-4 pt-6 border-t border-t-black'>
         <a className='font-mono text-md' href="https://x.com/Amaannn08">Reach Out!</a>    
-        <h1 className='font-mono text-md'>Made by Aman Gupta | 2025</h1>
+        <div className='flex flex-row items-center gap-1'>
+            <h1 className='font-mono text-md'>Made by Aman Gupta |</h1>
+            <img src="./images/copyright.png" className='h-3' alt="" />
+            <h1>2025</h1>
+        </div>
         <Clock></Clock>
     </div>
   )
